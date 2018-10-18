eddb4a5c2da4d5e4bc4e733eda52f5d707c8d2b2
胡秀雷 <8549744@qq.com>
2018-10-18 18:41:39 +0800

    new BaseActivity

---
+++ app/src/main/java/com/boco/testapp/BaseActivity.java
@@ -0,0 +1,13 @@
+package com.boco.testapp;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * <pre>
+ *   author : boco
+ *   time   : 2018/10/18
+ *   desc   :
+ * </pre>
+ */
+public class BaseActivity extends AppCompatActivity {
+}
